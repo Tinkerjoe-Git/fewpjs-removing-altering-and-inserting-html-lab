36cf40ff7749cde043fe09561ac78ce5b0425fad
tinkerjoe-git <nicholas.stagno@gmail.com>
2021-05-03 17:34:43 -0400

    commit tree 03426118f5d7631733abb4ab2231db099670b46d

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+document.querySelector('main#main').remove()
+
+
+
+let header = document.createElement("h1");
+header.setAttribute("id", "victory");
+document.body.appendChild(header)
+
+
+let newHeader = document.querySelector('h1#victory');
+newHeader.innerHTML = "Jean is the champion";
+
+
+
